982ad5419f64a3fc15851d4b7742ce6256bba081
Millisa G <millisa419@yahoo.com>
2025-08-04 12:30:57 -0400

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
     def create
         user = User.new(user_params)
         if user.save
-            render json: { message: "User created successfully" }, status: :created
+            token = JsonWebToken.encode(user_id: user.id)
+            render json: { user: user, token: token }, status: :created
         else
             render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
         end
